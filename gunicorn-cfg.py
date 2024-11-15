b86dc66bd88b1a2f3b02e7d45b37f47884c27e3d
Natalia <natalia.o.dolynska@gmail.com>
2024-11-15 17:04:32 +0100

    Updated app

--- gunicorn-cfg.py
+++ gunicorn-cfg.py
@@ -3,9 +3,13 @@
 Copyright (c) 2019 - present AppSeed.us
 """
 
-bind = '0.0.0.0:80'
+bind = '0.0.0.0:443'  # Use port 443 for HTTPS
 workers = 1
 accesslog = '-'
 loglevel = 'debug'
 capture_output = True
 enable_stdio_inheritance = True
+
+# SSL Configuration
+ssl_certfile = 'localhost.crt'  # Replace with the correct path to your SSL certificate
+ssl_keyfile = 'localhost.key'
